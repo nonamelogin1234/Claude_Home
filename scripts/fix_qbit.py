ebf5c01e186c67c68293cf5e23eac5a7a7a7a16e
nonamelogin1234 <nonamelogin@mail.ru>
2026-04-14 20:08:00 +0300

    set permanent qbittorrent password

--- scripts/fix_qbit.py
+++ scripts/fix_qbit.py
@@ -1,8 +1,27 @@
+import hashlib, os, re
+
 path = '/home/sergei/qbittorrent/config/qBittorrent/qBittorrent.conf'
+
+password = '4815'
+salt = os.urandom(16).hex()
+h = hashlib.pbkdf2_hmac('sha512', password.encode(), salt.encode(), 100000)
+password_hash = '@ByteArray(' + h.hex() + ':' + salt + ')'
+
 with open(path, 'r') as f:
     content = f.read()
-if 'HostHeaderValidation' not in content:
-    content = content.replace('WebUI\\Port=8090', 'WebUI\\Port=8090\nWebUI\\HostHeaderValidation=false\nWebUI\\CSRFProtection=false')
+
+# Remove old password lines if any
+content = re.sub(r'WebUI\\Password_PBKDF2.*\n', '', content)
+content = re.sub(r'WebUI\\Password_ha1.*\n', '', content)
+
+# Insert after WebUI\Port line
+content = content.replace(
+    'WebUI\\Port=8090',
+    'WebUI\\Password_PBKDF2="' + password_hash + '"\nWebUI\\Port=8090'
+)
+
 with open(path, 'w') as f:
     f.write(content)
-print('done')
+
+print('Password set OK')
+print('Hash:', password_hash[:40], '...')
